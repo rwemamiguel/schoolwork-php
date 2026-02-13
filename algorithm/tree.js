149e598b7e4c525661f083555082066f0e52d71d
manzi12 <manziivan810@gmail.com>
2026-02-13 01:54:59 -0800

    Tree working

--- algorithm/tree.js
+++ algorithm/tree.js
@@ -9,17 +9,51 @@ class binarytree{
     constructor(){
         this.root=null
     }
-};
-function insert (value){
-    const newnode=new binarynode(value);
+
+insert(value){
+    const node=new binarynode(value);
     if(!this.root){
-        this.root=newnode;
+        this.root=node;
         return;
     }
     let current=this.root;
     while(current){
-        if(current){
-
+        if(value < current.value){
+            if(current.left ==null){
+                current.left=node;
+                break;
+            }
+            current=current.left;
+        }else{
+            if(current.right ===null){
+                current.right=node;
+                break;
+            }
+            current=current.right;
         }
+
     }
 }
+Show(){
+        const tree=[]
+        let current=this.root;
+        while(tree.length>0|| current){
+            while(current){
+                tree.push(current);
+                current=current.left
+            }
+            current=tree.pop();
+            console.log(current.value);
+            current=current.right
+        }
+    }
+
+}
+const tree = new binarytree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(15);
+tree.insert(3);
+tree.insert(7);
+
+tree.Show();  
